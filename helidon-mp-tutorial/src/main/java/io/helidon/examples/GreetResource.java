ce95982f0a567343163f25ca41c2b6dbd49d60a4
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-06-29 11:12:54 +0900

    add Config

--- helidon-mp-tutorial/src/main/java/io/helidon/examples/GreetResource.java
+++ helidon-mp-tutorial/src/main/java/io/helidon/examples/GreetResource.java
@@ -3,6 +3,7 @@ package io.helidon.examples;
 import java.util.Map;
 
 import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonBuilderFactory;
 import jakarta.json.JsonObject;
@@ -16,12 +17,24 @@ import jakarta.ws.rs.core.MediaType;
 public class GreetResource {
 
     private static final JsonBuilderFactory JSON = Json.createBuilderFactory(Map.of());
+    private final GreetingProvider greetingProvider;
+
+    @Inject
+    public GreetResource(GreetingProvider greetingProvider) {
+        this.greetingProvider = greetingProvider;
+    }
+
     @GET
     @Produces(value = MediaType.APPLICATION_JSON)
     public JsonObject getDefaultMessage() { 
+        return createResponse("World");
+    }
+
+    private JsonObject createResponse(String who) {
+        var message = String.format("%s, %s!", greetingProvider.getMessage(), who);
         return JSON.createObjectBuilder()
-                .add("message", "Hello World")
-                .build(); 
+                .add("message", message)
+                .build();
     }
 
 }
