6cd93a34e95fc1b602b777bc27e6d3581f0f3356
gbz3 <1808483+gbz3@users.noreply.github.com>
2025-06-29 11:32:09 +0900

    add PUT

--- helidon-mp-tutorial/src/main/java/io/helidon/examples/GreetResource.java
+++ helidon-mp-tutorial/src/main/java/io/helidon/examples/GreetResource.java
@@ -7,10 +7,14 @@ import jakarta.inject.Inject;
 import jakarta.json.Json;
 import jakarta.json.JsonBuilderFactory;
 import jakarta.json.JsonObject;
+import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 
 @Path("/greet") 
 @RequestScoped 
@@ -34,6 +38,33 @@ public class GreetResource {
         var message = String.format("%s, %s!", greetingProvider.getMessage(), who);
         return JSON.createObjectBuilder()
                 .add("message", message)
+                .build();
+    }
+
+    @Path("/{name}")
+    @GET
+    @Produces(value = MediaType.APPLICATION_JSON)
+    public JsonObject getMessage(@PathParam("name") String name) {
+        return createResponse(name);
+    }
+
+    @Path("/greeting")
+    @PUT
+    @Produces(value = MediaType.APPLICATION_JSON)
+    @Consumes(value = MediaType.APPLICATION_JSON)
+    public Response updateGreeting(JsonObject jsonObject) {
+        if (!jsonObject.containsKey("greeting")) {
+            var entity = JSON.createObjectBuilder()
+                    .add("error", "Missing 'greeting' property")
+                    .build();
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .entity(entity)
+                    .build();
+        }
+
+        var greeting = jsonObject.getString("greeting");
+        greetingProvider.setMessage(greeting);
+        return Response.status(Response.Status.NO_CONTENT)
                 .build();
     }
 
